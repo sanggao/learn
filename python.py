61d31dbb960ad921a17fea316bc3ce34e567996e
zhongyi8 <zhongyi8@staff.weibo.com>
2018-05-19 15:29:35 +0800

    111

--- python.py
+++ python.py
@@ -1,1 +1,8 @@
+<<<<<<< HEAD
 helle world  im a  better human
+=======
+add utf-8 encoding
+#use/bin/local python
+hdasdasdssssssssssssssssssssssahhahahahahhahahh
+asdaspppasdasdasdddddd:
+>>>>>>> a2450304db0e2815a13494c39fa91a119d793816
